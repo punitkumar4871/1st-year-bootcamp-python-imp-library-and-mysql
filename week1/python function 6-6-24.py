79b2ce2f6834f380c8128d55cad359822193bf8e
Punit Kumar <152253490+punitkumar4871@users.noreply.github.com>
2024-06-06 18:22:59 +0530

    done python function 6-6-24.py

--- week1/python function 6-6-24.py
+++ week1/python function 6-6-24.py
@@ -1,1 +1,6 @@
-
+# 1.Functions in Python
+'''user defined function in python are function which are made by user with their specific user codes basically these function are made to decrease time and to decrease 
+complexity of a program by allowing user to  developers to break down complex tasks into smaller, more manageable pieces, making the code easier to understand, maintain, and debug.
+'''
+properites of function:-
+function
