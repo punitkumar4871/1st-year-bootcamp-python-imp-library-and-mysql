8e5ae0afc6afd76ad1b86e738c362c48fbeb7725
punitkumar4871 <punitkumar4871@gmail.com>
2024-06-06 16:35:15 +0530

    done

---
+++ data data type.py
@@ -0,0 +1,18 @@
+# data type
+# Numeric types (int, float, complex) are immutable, meaning their values cannot be changed after creation.
+# Sequence types (str, list, tuple) can contain multiple values of different data types.
+# range is a special sequence type that generates a sequence of numbers within a specified range.
+# Mapping types (dict) store key-value pairs, where keys must be unique and immutable.
+# Set types (set, frozenset) are unordered collections of unique elements.
+# The bool type represents truth values (True or False).'''
+int_num = 42
+float_num = 3.14159
+complex_num = 1 + 2j
+string = "Python is awesome!"
+boolean = True
+list_example = [10, "text", 3.14, [1, 2, 3]]
+tuple_example = (20, "tuple", 7.5)
+range_example = range(1, 10)
+dict_example = {"name": "Alice", "age": 25, "city": "Wonderland"}
+set_example = {1, 2, 3, 4, 5}
+frozenset_example = frozenset([1, 2, 3, 4, 5])
