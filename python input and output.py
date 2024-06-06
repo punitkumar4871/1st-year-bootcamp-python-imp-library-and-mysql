a1a276666d99daed2657c0a05ace56a4cb2b2305
punitkumar4871 <punitkumar4871@gmail.com>
2024-06-06 16:26:35 +0530

    added one file

---
+++ python input and output.py
@@ -0,0 +1,23 @@
+# Input and Output in Python
+# output function
+''' output function in python:- This function allows us to display text, variables.'''
+# note :- if u want to print same expression like u want put it in "" and if u want to print variable value then no need to do that
+'''example'''
+print("Hello, World!")
+'''example2'''
+name = "Alice"
+age = 30
+print("Name:", name, "Age:", age)
+
+# input function:-
+### input function allow user to give input 
+''' disadvantage:- take every function in form of string i.e 3+3 =33'''
+#### int function it is used to give value in form interger
+
+# Taking input from the user
+num = int(input("Enter a value: "))
+
+add = num + 5
+
+# Output
+print("The sum is %d" %add)
