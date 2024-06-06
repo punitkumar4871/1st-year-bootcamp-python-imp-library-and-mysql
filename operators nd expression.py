dcc5af83142c1bd0f233111ebb5c8392a926581c
punitkumar4871 <punitkumar4871@gmail.com>
2024-06-06 16:46:21 +0530

    done

---
+++ operators nd expression.py
@@ -0,0 +1,54 @@
+# operators:- in python are used to do certain operations in pythons
+
+# An expression:- in Python is a combination of values, variables, operators, and function calls that are evaluated to produce a new value. Expressions are fundamental building blocks in Python programming, allowing you to perform calculations, manipulate data, and determine control flow.
+
+# Expressions and Operators
+a = 10
+b = 3
+
+# Arithmetic Operators
+print(a + b)  # Addition: 13
+print(a - b)  # Subtraction: 7
+print(a * b)  # Multiplication: 30
+print(a / b)  # Division: 3.3333333333333335
+print(a // b) # Floor Division: 3
+print(a % b)  # Modulus: 1
+print(a ** b) # Exponentiation: 1000
+
+# Comparison Operators
+print(a == b) # Equal to: False
+print(a != b) # Not equal to: True
+print(a > b)  # Greater than: True
+print(a < b)  # Less than: False
+print(a >= b) # Greater than or equal to: True
+print(a <= b) # Less than or equal to: False
+
+# Logical Operators
+print(a > 5 and b < 5)  # and: True
+print(a > 5 or b < 1)   # or: True
+print(not(a > 5 and b < 5)) # not: False
+
+# code:-
+a = 7
+b = 2
+
+# addition
+print ('Sum: ', a + b)  
+
+# subtraction
+print ('Subtraction: ', a - b)   
+
+# multiplication
+print ('Multiplication: ', a * b)  
+
+# division
+print ('Division: ', a / b) 
+
+# floor division
+print ('Floor Division: ', a // b)
+
+# modulo
+print ('Modulo: ', a % b)  
+
+# a to the power b
+print ('Power: ', a ** b)   
