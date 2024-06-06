b522d1437d3ab1ed43833b726c03a2fe6b010bb3
punitkumar4871 <punitkumar4871@gmail.com>
2024-06-06 16:38:19 +0530

    done

--- data data type.py
+++ data data type.py
@@ -5,14 +5,15 @@
 # Mapping types (dict) store key-value pairs, where keys must be unique and immutable.
 # Set types (set, frozenset) are unordered collections of unique elements.
 # The bool type represents truth values (True or False).'''
-int_num = 42
-float_num = 3.14159
-complex_num = 1 + 2j
-string = "Python is awesome!"
-boolean = True
-list_example = [10, "text", 3.14, [1, 2, 3]]
-tuple_example = (20, "tuple", 7.5)
-range_example = range(1, 10)
-dict_example = {"name": "Alice", "age": 25, "city": "Wonderland"}
-set_example = {1, 2, 3, 4, 5}
-frozenset_example = frozenset([1, 2, 3, 4, 5])
+int_num = 42      # these are interger
+float_num = 3.14159    # float contain decimals
+complex_num = 1 + 2j    # it contain complex number as root -1
+
+string = "Python is awesome!"    # it sontain string value
+boolean = True  # boolean contain true and false
+list_example = [10, "text", 3.14, [1, 2, 3]]    # it contain valuein [] these brakcets
+tuple_example = (20, "tuple", 7.5)   # this contain value in ()
+range_example = range(1, 10)       # this contain value form intial range to final range
+dict_example = {"name": "Alice", "age": 25, "city": "Wonderland"}    # it conatin value stored form key and value
+set_example = {1, 2, 3, 4, 5}   # contain in {}
+frozenset_example = frozenset([1, 2, 3, 4, 5])    # contian in mix brakcet format
